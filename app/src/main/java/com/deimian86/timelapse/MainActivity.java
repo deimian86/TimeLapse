19d314a6e4aa64006730287f1eba1f9235ef1bb9
deimian86 <deimian86@gmail.com>
2017-07-23 01:44:52 +0200

    Alpha basic function

--- app/src/main/java/com/deimian86/timelapse/MainActivity.java
+++ app/src/main/java/com/deimian86/timelapse/MainActivity.java
@@ -3,6 +3,8 @@ package com.deimian86.timelapse;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView txtView = (TextView) findViewById(R.id.txtView);
-        txtView.setText(new TimeLapse().helloWorld());
+        TextView txtView1 = findViewById(R.id.txtView1);
+        txtView1.setText(new TimeLapse().getLapse(getApplicationContext(), minutesAgo(5)));
+
+        TextView txtView2 = findViewById(R.id.txtView2);
+        txtView2.setText(new TimeLapse().getLapse(getApplicationContext(), minutesAgo(15)));
+
+        TextView txtView3 = findViewById(R.id.txtView3);
+        txtView3.setText(new TimeLapse().getLapse(getApplicationContext(), hoursAgo(1)));
+
+        TextView txtView4 = findViewById(R.id.txtView4);
+        txtView4.setText(new TimeLapse().getLapse(getApplicationContext(), hoursAgo(3)));
+
+        TextView txtView5 = findViewById(R.id.txtView5);
+        txtView5.setText(new TimeLapse().getLapse(getApplicationContext(), daysAgo(1)));
+
+        TextView txtView6 = findViewById(R.id.txtView6);
+        txtView6.setText(new TimeLapse().getLapse(getApplicationContext(), daysAgo(3)));
+
+        TextView txtView7 = findViewById(R.id.txtView7);
+        txtView7.setText(new TimeLapse().getLapse(getApplicationContext(), daysAgo(7)));
+
+        TextView txtView8 = findViewById(R.id.txtView8);
+        txtView8.setText(new TimeLapse().getLapse(getApplicationContext(), daysAgo(21)));
+    }
+
+    private Date daysAgo(int i) {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -i);
+        return cal.getTime();
+    }
+
+    private Date hoursAgo(int i) {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.HOUR_OF_DAY, -i);
+        return cal.getTime();
+    }
+
+    private Date minutesAgo(int i) {
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.MINUTE, -i);
+        return cal.getTime();
     }
 }
--- time-lapse/src/main/java/com/deimian86/timelapse/TimeLapse.java
+++ time-lapse/src/main/java/com/deimian86/timelapse/TimeLapse.java
@@ -1,12 +1,52 @@
 package com.deimian86.timelapse;
 
+import android.content.Context;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by deimian86 on 19/07/2017.
  */
-
 public class TimeLapse {
 
-    public String helloWorld(){
-        return "Hola, Soy TimeLapse";
+    public String getLapse(Context context, Date fechaInicial) {
+        String cadena = "";
+        SimpleDateFormat formatoDelTexto = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date fechaActual = new Date();
+        formatoDelTexto.format(fechaActual);
+        // Tenemos la fecha inicial y la actual ahora operamos con ellas
+        long diff = fechaActual.getTime() - fechaInicial.getTime();
+        long diffMinutes = diff / (60 * 1000) % 60;
+        long diffHours = diff / (60 * 60 * 1000) % 24;
+        long diffDays = diff / (24 * 60 * 60 * 1000);
+        // Formateamos la cadena
+        if (diffDays == 0) {
+            if (diffHours == 0) {
+                if (diffMinutes < 10) {
+                    cadena = context.getResources().getString(R.string.lapse_moments_ago);
+                } else {
+                    cadena = context.getResources().getString(R.string.lapse_minutes_ago, diffMinutes);
+                }
+            } else if (diffHours == 1) {
+                cadena = context.getResources().getString(R.string.lapse_hour_ago, diffHours);
+            } else if (diffHours > 1) {
+                cadena = context.getResources().getString(R.string.lapse_hours_ago, diffHours);
+            }
+        } else if (diffDays == 1) {
+            cadena = context.getResources().getString(R.string.lapse_day_ago, diffDays);
+        } else if (diffDays > 1 && diffDays < 7) {
+            cadena = context.getResources().getString(R.string.lapse_days_ago, diffDays);
+        } else if (diffDays == 7) {
+            cadena = context.getResources().getString(R.string.lapse_week_ago);
+        } else if (diffDays > 7 && diffDays < 14) {
+            cadena = context.getResources().getString(R.string.lapse_days_ago, diffDays);
+        } else if (diffDays == 14) {
+            cadena = context.getResources().getString(R.string.lapse_weeks_ago);
+        } else if (diffDays > 14) {
+            SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+            cadena = formato.format(fechaInicial);
+        }
+        return cadena;
     }
 }
